e559f64a9f9e45187dfb31bdfb13f2899fbbdb87
Tanmai Khanna <khanna.tanmai@gmail.com>
2019-03-03 09:22:42 +0530

    fixing byte anaphora, first LU appears now and slashes too

--- byte_anaphora.py
+++ byte_anaphora.py
@@ -1,66 +1,69 @@
 from streamparser import parse
-
+import sys
 import time
+
 start = time.time()
 
 count = 0
 
-with open("input.txt", "rb") as f:
-    byte = f.read(1)
+with sys.stdin as f:
 
     input_stream = ''
     last_noun = ''
     flag = 0
 
+    byte = f.read(1)
+    while byte:
 
-    while byte != b'':
-        # Do stuff with byte.
-        byte = f.read(1)
-
-        byte_str = byte.decode("utf-8", "ignore") #Can Edit whichever UNICODE is followed by Apertium
-        #Currently NOT reading ó character - Not sure why, As it Exists in utf-8
-
-        if(byte_str == '^'):
-                flag = 1 #processing begins
+        if(byte == '^'):
+            flag = 1 #processing begins
 
         if(flag == 1):
-                input_stream += str(byte_str) #add to input stream
+            input_stream += str(byte) #add to input stream
 
-        if(byte_str == '$'): #token processed
-                flag = 0
-                count += 1
+        if(byte == '$'): #token processed
+            flag = 0
+            count += 1
 
-                #print(str(input_stream))
+            #print(str(input_stream))
 
-                lexical_units = parse(input_stream)
-                output_stream = ''
+            lexical_units = parse(input_stream)
+            output_stream = ''
 
-                for lexical_unit in lexical_units: #Now this should be only one lexical unit -- can be changed
-                        output_stream += '^' + str(lexical_unit)
-                        #print(lexical_unit)
+            for lexical_unit in lexical_units: #Now this should be only one lexical unit -- can be changed
+                output_stream += '^' + str(lexical_unit) + '/'
+                #print(lexical_unit)
 
-                        if(len(lexical_unit.readings[0][0][1]) > 0):
-                        	#print(lexical_unit.readings[0][0][1][0])
+                if(len(lexical_unit.readings[0][0][1]) > 0):
+                	#print(lexical_unit.readings[0][0][1][0])
 
-                        	if(lexical_unit.readings[0][0][1][0] == 'n'):
-                        		last_noun = str(lexical_unit)
-                        		last_noun = last_noun.split('/')[-1] #Taking only the last option (the one in English)
+                	if(lexical_unit.readings[0][0][1][0] == 'n'):
+                		last_noun = str(lexical_unit)
+                		last_noun = last_noun.split('/')[-1] #Taking only the last option (the one in English)
 
-                        	tags = lexical_unit.readings[0][0][1]
+                	tags = lexical_unit.readings[0][0][1]
 
-                        	if(('det' in tags and 'pos' in tags) or ('prn' in tags)): #If it's a determiner and possessive, it needs anaphora resolution, can add other tags here
-                        		output_stream += '/' + last_noun #If there's no last noun, then nothing will be concatenated
+                	if(('det' in tags and 'pos' in tags) or ('prn' in tags)): #If it's a determiner and possessive, it needs anaphora resolution, can add other tags here
+                		output_stream += last_noun #If there's no last noun, then nothing will be concatenated
 
-                        output_stream += '$ '
+                output_stream += '$ '
 
-                        #print(last_noun)
+                #print(last_noun)
 
-                print(output_stream) #Uncomment to see output
+            #print(output_stream) #Uncomment to see output
+            sys.stdout.write(output_stream) #Uncomment to see output
 
-                input_stream = ''
+            input_stream = ''
+
+        byte = f.read(1)
+
 
 end = time.time()
-print("Time Taken:")
-print(end - start)
+#print("Time Taken:")
+#print(end - start)
 
-print("Total Token Count" + str(count))
+print("Time Taken:", file=sys.stderr)
+print(end - start, file=sys.stderr)
+
+#print("Total Token Count" + str(count))
+print("Total Token Count:" + str(count), file=sys.stderr)
