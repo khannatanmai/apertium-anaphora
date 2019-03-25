5e71067ec5544a01e4b17afb05f43fd96075b841
Tanmai Khanna <khanna.tanmai@gmail.com>
2019-03-25 20:51:00 +0530

    Arranging Repo

--- sbtest.py
+++
@@ -1,6 +0,0 @@
-from streamparser import parse_file, reading_to_string
-import sys
-for blank, lu in parse_file(sys.stdin, with_text=True):
-    print(blank+" ".join("^{}/{}$".format(lu.wordform, reading_to_string(r))
-                         for r in lu.readings),
-          end="")
